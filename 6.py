4a0cb7a8854d4d441afc0fafabfc5290c667a16b
PrinceCaspian <princecaspian@duck.com>
2023-12-07 13:01:43 -0600

    Day 6

--- 6.py
+++ 6.py
@@ -2,6 +2,7 @@
 
 import pathlib
 import sys
+import re
 
 def parse(puzzle_input):
     """Parse input."""
@@ -17,10 +18,53 @@ def parse(puzzle_input):
 
 def part1(numbers):
     """Solve part 1."""
+    time = list(map(int, (re.split("\s+", numbers[0])[1:])))
+    dist = list(map(int, (re.split("\s+", numbers[1])[1:])))
+
+
+    pairs = [(time[i], dist[i]) for i in range(len(time))]
+
+    a = []
+
+    for pr in pairs:
+        s = 0
+        for i in range(pr[0]):
+            distance = (pr[0] - i)*i
+            if(distance > pr[1]):
+                s+= 1
+        a.append(s)
+    
+    k = 1
+    for r in a:
+        k *= r
+    return k
+
     print("EO1____________")
 
 def part2(numbers):
     """Solve part 2."""
+    time = list(map(int, (re.split("\s+", numbers[0])[1:])))
+    time = [int(''.join(list(map(str, time))))]
+    dist = list(map(int, (re.split("\s+", numbers[1])[1:])))
+    dist = [int(''.join(list(map(str, dist))))]
+
+    pairs = [(time[i], dist[i]) for i in range(len(time))]
+
+    a = []
+
+    for pr in pairs:
+        s = 0
+        for i in range(pr[0]):
+            distance = (pr[0] - i)*i
+            if(distance > pr[1]):
+                s+= 1
+        a.append(s)
+    
+    k = 1
+    for r in a:
+        k *= r
+    return k
+
     print("EO2____________")
 
 if __name__ == "__main__":
