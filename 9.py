fa4d46bd8e23bf1e1b68b12d0319363c552fab19
PrinceCaspian <princecaspian@duck.com>
2023-12-08 23:22:54 -0600

    Day 9

--- 9.py
+++ 9.py
@@ -8,6 +8,7 @@ def parse(puzzle_input):
     # Read each line as a string
     file = open(f'{puzzle_input}.txt',mode='r')
     inp = file.read().split("\n")
+    inp = [list(map(int, line.split(" "))) for line in inp]
     file.close()
 
     # Convert each line to an integer 
@@ -17,10 +18,35 @@ def parse(puzzle_input):
 
 def part1(numbers):
     """Solve part 1."""
+    s = 0
+    for line in numbers:
+        while not all([n == 0 for n in line]):
+              s += line[-1]
+              line = [line[n+1]-line[n] for n in range(len(line) - 1)]
+    return s
     print("EO1____________")
 
 def part2(numbers):
     """Solve part 2."""
+    s = 0
+    for line in numbers:
+        hist = []
+        while not all([n == 0 for n in line]):
+            hist += [line]
+            line = [line[n+1]-line[n] for n in range(len(line) - 1)]
+        hist += [line]
+        
+        hist = hist[::-1]
+
+        hist[0].append(0)
+
+        for i in range(1, len(hist)):
+            hist[i] = [hist[i][0] - hist[i-1][0]] + hist[i]
+        
+        s += hist[-1][0]
+    
+
+    return s
     print("EO2____________")
 
 if __name__ == "__main__":
