ce878ffaed95dc0571e9d57ce8f5d230f72db997
PrinceCaspian <princecaspian@duck.com>
2023-12-11 19:39:43 -0600

    Day 11

--- 11.py
+++ 11.py
@@ -1,7 +1,8 @@
-# aoc 2023 _.py
+# aoc 2023 11.py
 
 import pathlib
 import sys
+import re
 
 def parse(puzzle_input):
     """Parse input."""
@@ -17,10 +18,60 @@ def parse(puzzle_input):
 
 def part1(numbers):
     """Solve part 1."""
+    rows = [all([i != "#" for i in k]) for k in numbers]
+    transposed = [[numbers[j][i] for j in range(len(numbers))] for i in range(len(numbers[0]))]
+    cols = [all([i != "#" for i in k]) for k in transposed]
+
+    s = 0
+
+    pairs = []
+
+    for x, i in enumerate(numbers):
+        for y, j in enumerate(i):
+            if(j == "#"):
+                pairs.append((x, y))
+    #print(pairs)
+
+    for pair in pairs:
+        for other in pairs:
+            diff = abs(other[0]-pair[0]) + abs(other[1]-pair[1])
+            #re = rows[min(other[0], pair[0]):max(other[0], pair[0])]
+            numberRowExpansions = sum(rows[min(other[0], pair[0]):max(other[0], pair[0])])
+            #ce = cols[min(other[1], pair[1]):max(other[1], pair[1])]
+            numberColExpansions = sum(cols[min(other[1], pair[1]):max(other[1], pair[1])])
+
+            #print(pair, other, sum([diff, numberRowExpansions, numberColExpansions]))
+            s += sum([diff, numberRowExpansions, numberColExpansions])
+    
+    return s//2
     print("EO1____________")
 
 def part2(numbers):
     """Solve part 2."""
+    rows = [all([i != "#" for i in k]) for k in numbers]
+    transposed = [[numbers[j][i] for j in range(len(numbers))] for i in range(len(numbers[0]))]
+    cols = [all([i != "#" for i in k]) for k in transposed]
+
+    s = 0
+
+    pairs = []
+
+    for x, i in enumerate(numbers):
+        for y, j in enumerate(i):
+            if(j == "#"):
+                pairs.append((x, y))
+
+    for pair in pairs:
+        for other in pairs:
+            diff = abs(other[0]-pair[0]) + abs(other[1]-pair[1])
+            numberRowExpansions = sum(rows[min(other[0], pair[0]):max(other[0], pair[0])])
+            numberColExpansions = sum(cols[min(other[1], pair[1]):max(other[1], pair[1])])
+
+            mult = (1E6) -1 
+
+            s += sum([diff, numberRowExpansions*mult, numberColExpansions*mult])    
+
+    return s//2
     print("EO2____________")
 
 if __name__ == "__main__":
