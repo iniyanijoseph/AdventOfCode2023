9924874356e497e1b31d079e82982946074a462d
PrinceCaspian <princecaspian@duck.com>
2023-12-15 10:49:51 -0600

    Day 15

--- 15.py
+++ 15.py
@@ -7,7 +7,7 @@ def parse(puzzle_input):
     """Parse input."""
     # Read each line as a string
     file = open(f'{puzzle_input}.txt',mode='r')
-    inp = file.read().split("\n")
+    inp = file.read().split(",")
     file.close()
 
     # Convert each line to an integer 
@@ -17,10 +17,58 @@ def parse(puzzle_input):
 
 def part1(numbers):
     """Solve part 1."""
+    s = 0
+    for i in numbers:
+        cval = 0
+        for j in i:
+            cval += ord(j)
+            cval *= 17
+            cval %= 256
+        s += cval
+
+    return s
     print("EO1____________")
 
 def part2(numbers):
     """Solve part 2."""
+    boxes = {}
+    for i in numbers:
+        if "-" in i:
+            code, _ = i.split("-")
+            for k in boxes:
+                for ind, r in enumerate(boxes[k]):
+                    if r[0] == code: 
+                        boxes[k].pop(ind)
+                
+        else:
+            code, val = i.split("=")
+            cval = 0
+            for j in code:
+                cval += ord(j)
+                cval *= 17
+                cval %= 256
+            
+            foundit = False
+            if not cval in boxes:
+                boxes[cval] = [(code, val)]
+            else:
+                for ind, r in enumerate(boxes[cval]):
+                    if r[0] == code: 
+                        boxes[cval][ind] = (code, val)
+                        foundit = True
+                if not foundit:
+                    boxes[cval].append((code, val))
+
+
+    s = 0
+
+    print(boxes)
+
+    for k in boxes:
+        for ind, r in enumerate(boxes[k]):
+            s += (k+1)*(ind+1)*int(r[1])
+
+    return s
     print("EO2____________")
 
 if __name__ == "__main__":
