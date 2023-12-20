c89b122caf5afaa6276b1e261ea88ec14f50f1fe
PrinceCaspian <princecaspian@duck.com>
2023-12-19 19:12:23 -0600

    Day 19 p1

--- 19.py
+++ 19.py
@@ -1,13 +1,12 @@
 # aoc 2023 19.py
 
-import pathlib
-import sys
+import re
 
 def parse(puzzle_input):
     """Parse input."""
     # Read each line as a string
     file = open(f'{puzzle_input}.txt',mode='r')
-    inp = file.read().split("\n")
+    inp = file.read().split("\n\n")
     file.close()
 
     # Convert each line to an integer 
@@ -17,6 +16,54 @@ def parse(puzzle_input):
 
 def part1(numbers):
     """Solve part 1."""
+    workflows = numbers[0].split("\n")
+    part = numbers[1].split("\n")
+
+    flows = [re.split('{|}|,',line) for line in workflows]
+    
+    keys = {}
+    
+    for line in flows:
+        key = line[0]
+        order = []
+        for ele in line[1:-2]:
+            k, sign, value, target = (re.split('<|>', ele.split(":")[0])[0], -1 if '<' in ele else 1, re.split('<|>', ele.split(":")[0])[1], ele.split(":")[1])
+            order.append([k, sign, int(value), target]) 
+
+        default = line[-2]
+
+        order.append(['', -1, 10**10, default])
+
+        keys[key] = order    
+
+    part = [[r for r in re.split('{|}|,', line) if r != ""] for line in part]
+    prt = []
+    for k in part:
+        temp = {}
+        for r in k:
+            temp[r.split("=")[0]] = int(r.split("=")[1])
+        prt.append(temp)
+
+    s = 0
+    for task in prt:
+        current = "in"
+        while(1):
+            for k in keys[current] if not current in ["A", "R"] else []:
+                key, sign, value, target = k
+                if key != '':
+                    v = task[key]
+                    if (v*sign > value * sign):
+                        current = target
+                        break
+                else:
+                    current = target
+                    break
+            if(current == "A"):
+                s += sum(task.values())
+                break
+            if(current == "R"):
+                break
+    return s
     print("EO1____________")
 
 def part2(numbers):
